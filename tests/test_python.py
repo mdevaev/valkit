4d31b9a13693c69959f9f1af2b2e434d32f2b98a
Devaev Maxim <mdevaev@gmail.com>
2016-10-27 20:26:21 +0300

    added python.valid_object_path() and python.valid_logging_level()

--- tests/test_python.py
+++ tests/test_python.py
@@ -1,7 +1,11 @@
+import logging
+
 import pytest
 
 from valkit import ValidatorError
 from valkit.python import valid_object_name
+from valkit.python import valid_object_path
+from valkit.python import valid_logging_level
 
 
 # =====
@@ -21,3 +25,42 @@ def test_fail__valid_object_name():
         print(arg)
         with pytest.raises(ValidatorError):
             valid_object_name(arg)
+
+
+# =====
+def test_ok__valid_object_path():
+    for arg in [
+        "__test",
+        "__test__",
+        "Object_Name",
+        "objectName123",
+        "_",
+        "__test.test",
+        "_._._",
+    ]:
+        assert valid_object_path(arg) == arg
+
+
+def test_fail__valid_object_path():
+    for arg in [".object", "123object", "/object", "a.b.c." "a.1x.b"]:
+        print(arg)
+        with pytest.raises(ValidatorError):
+            valid_object_path(arg)
+
+
+# =====
+def test_ok__valid_logging_level():
+    for arg in logging._levelToName:  # pylint: disable=protected-access
+        assert valid_logging_level(arg) == arg
+        assert valid_logging_level(str(arg)) == arg
+
+    for (arg, retval) in logging._nameToLevel.items():  # pylint: disable=protected-access
+        assert valid_logging_level(arg) == retval
+        assert valid_logging_level(arg.lower()) == retval
+
+
+def test_fail__valid_logging_level():
+    for arg in ["foo", "bar", "111", 111, "info"]:
+        print(arg)
+        with pytest.raises(ValidatorError):
+            valid_logging_level(arg, up=False)
--- valkit/python.py
+++ valkit/python.py
@@ -1,3 +1,5 @@
+import logging
+
 from . import _tools
 
 
@@ -5,3 +7,25 @@ from . import _tools
 @_tools.add_lambda_maker
 def valid_object_name(arg, strip=False):
     return _tools.check_re_match(arg, "Python object name", r"^[a-zA-Z_][a-zA-Z0-9_]*$", strip)
+
+
+@_tools.add_lambda_maker
+def valid_object_path(arg, strip=False):
+    pattern = r"^([a-zA-Z_][a-zA-Z0-9_]*\.)*[a-zA-Z_][a-zA-Z0-9_]*$"
+    return _tools.check_re_match(arg, "Python object path", pattern, strip)
+
+
+@_tools.add_lambda_maker
+def valid_logging_level(arg, up=True, strip=False):
+    name = "logging level"
+    try:
+        arg = int(arg)
+        if arg not in logging._levelToName:  # pylint: disable=protected-access
+            _tools.raise_error(arg, name)
+        return arg
+    except ValueError:
+        arg = _tools.not_none_string(arg, name, strip)
+        try:
+            return logging._nameToLevel[arg.upper() if up else arg]  # pylint: disable=protected-access
+        except KeyError:
+            _tools.raise_error(arg, name)
